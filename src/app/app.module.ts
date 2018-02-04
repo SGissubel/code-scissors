f8b774e62e6dc78aeaf74030519b5fb7833ae8f4
Steven Gissubel <steven.gissubel@gmail.com>
2018-02-04 15:46:18 -0500

    added mdbootstrap and styling / added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+// components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+
+// directives 
+import { DropdownDirective } from './shared/dropdown.directive';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    DropdownDirective,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+    ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,1 +1,20 @@
-dropdown.directive.ts
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document:click', ['$event']) toggleOpen(e: Event) {
+    const dropdown = this.elemRef.nativeElement.contains(e.target);
+    if (dropdown) {
+      this.isOpen = !this.isOpen;
+    } else {
+      this.isOpen = false;
+    }
+  }
+
+
+  constructor(private elemRef: ElementRef) {}
+}
