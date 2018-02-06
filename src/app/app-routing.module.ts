61a8965e116a8d68bbb8be04376667622eaa1be0
Steven Gissubel <steven.gissubel@gmail.com>
2018-02-06 07:31:04 -0500

    moved routes into app-routing.module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// components
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login/login.component';
+import { SignupComponent } from './login/signup/signup.component';\
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
-
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 // components
 import { AppComponent } from './app.component';
@@ -13,21 +12,13 @@ import { FooterComponent } from './nav/footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { BodyComponent } from './home-page/body/body.component';
 import { LoginComponent } from './login/login/login.component';
-import { SignupComponent } from './login/signup/signup.component';\
+import { SignupComponent } from './login/signup/signup.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 // directives 
 import { DropdownDirective } from './shared/dropdown.directive';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'not-found' }
-];
 
 @NgModule({
   declarations: [
@@ -46,8 +37,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes),
-    MDBBootstrapModule.forRoot()
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
     ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
