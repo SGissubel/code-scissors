ba4ad0783f4142b7b353dfec28ab6593a87cd135
Steven Gissubel <steven.gissubel@gmail.com>
2018-02-19 00:04:58 -0500

    styling login/signup

--- src/app/login/login-signup.component.ts
+++ src/app/login/login-signup.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
   styleUrls: ['./login-signup.component.scss']
 })
-export class LoginSignupComponent {
+export class LoginSignupComponent implements OnInit {
+  isNotLoginRoute: boolean = false;
+
+  constructor(private location: PlatformLocation) { }
+
+  ngOnInit() {
+    if (this.location.location.href.indexOf('signup') !== -1) {
+      this.isNotLoginRoute = true;
+    } 
+  }
 
 }
